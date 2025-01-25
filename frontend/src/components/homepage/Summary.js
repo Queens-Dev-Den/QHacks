ae05468ff6a31442e1f16e0b6479b5588e96a597
Nicholas Saloufakos <nsaloufakos@gmail.com>
2025-01-25 04:01:46 -0500

    Assistant box

--- frontend/src/components/homepage/Summary.js
+++ frontend/src/components/homepage/Summary.js
@@ -8,10 +8,9 @@ const Summary = () => {
             <h1>Workout</h1>
             <p>Push</p>
             <p>Swim</p>
-            <h1></h1>
             <h1>Meals</h1>
-            <p>Push</p>
-            <p>Swim</p>
+            <p>Banana Smoothie</p>
+            <p>Steak and Potatoes</p>
         </div>
     );
 };
