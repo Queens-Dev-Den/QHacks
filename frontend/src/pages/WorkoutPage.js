059d97ddfde9f5ceb7241f7e74f03bea1ab315de
Bouckley <jordanbouckley@gmail.com>
2025-01-25 16:09:50 -0500

    Style workout page

--- frontend/src/pages/WorkoutPage.js
+++ frontend/src/pages/WorkoutPage.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/WorkoutPage.css';
+import AssistantWorkout from '../components/workoutpage/AssistantWorkout';
 
 const WorkoutPage = () => {
 
   return (
     <div className="workoutpage-container">
-      <h1>Workout</h1>
+      <h1 className='workoutpage-title'>Workout</h1>
+      <AssistantWorkout />
     </div>
   );
 };
