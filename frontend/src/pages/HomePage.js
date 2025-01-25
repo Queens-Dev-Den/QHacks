7b4e86d6b4f59b871841b772c1943732adf5aecc
Nicholas Saloufakos <nsaloufakos@gmail.com>
2025-01-25 02:32:51 -0500

    Homepage edits

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/HomePage.css';
+import Assistant from '../components/homepage/Assistant';
+import Summary from '../components/homepage/Summary';
 
 const HomePage = () => {
   
   return (
     <div className="homepage-container">
-      <h1>Homepage</h1>
+      <h1 className="homepage-title">Today</h1>
+      <Assistant />
+      <Summary />
     </div>
   );
 };
